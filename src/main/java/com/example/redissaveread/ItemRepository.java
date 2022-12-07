03f5a42a4b8fc3d9b1b4171defa9a7acdd646f9b
YeonJun Cho <micayel1219@gmail.com>
2022-12-07 20:44:27 +0900

    feat : 리스트 조회 캐싱

--- src/main/java/com/example/redissaveread/ItemRepository.java
+++ src/main/java/com/example/redissaveread/ItemRepository.java
@@ -1,6 +1,8 @@
 package com.example.redissaveread;
 
+import com.example.redissaveread.config.CacheNames;
 import lombok.RequiredArgsConstructor;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.redis.core.RedisCallback;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.RedisSerializer;
@@ -9,12 +11,15 @@ import org.springframework.stereotype.Repository;
 import java.util.*;
 import java.util.concurrent.TimeUnit;
 
+import static com.example.redissaveread.config.CacheNames.POST;
+
 @Repository
 @RequiredArgsConstructor
 public class ItemRepository {
 //    private final RedisTemplate<String, Object> redisTemplate;
 //    private final RedisTemplate<String, RedisTemplate<String, Object>> carts;
     private final RedisTemplate<String, ItemDto> redisTemplate;
+//    @Cacheable(value = CacheNames.POST)
     public void addItem(ItemDto itemDto, Long memberId){
         String key = KeyGen.cartKeyGenerate(memberId);
 //        List<ItemDto> items = findByMemberId(memberId);
@@ -24,6 +29,7 @@ public class ItemRepository {
 //        redisTemplate.expire(key, 60, TimeUnit.SECONDS);
     }
 
+    @Cacheable(value = CacheNames.POST)
     public List<ItemDto> findByMemberId(Long memberId){
         String key = KeyGen.cartKeyGenerate(memberId);
         Long len = redisTemplate.opsForList().size(key);
